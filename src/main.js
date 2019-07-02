b76238c05e4ef217daf1268b94557d40a10a2236
Alex Siegberg and Garland Lai <garland@>
2019-07-02 11:35:16 -0700

    add weather API call with javascript to dislay humidity and degress in fahrenheit

--- src/main.js
+++ src/main.js
@@ -23,18 +23,41 @@ export let showDepressed = (pet) => {
 }
 
 $(document).ready(function() {
-   let userPet = new Pet;
+  let userPet = new Pet;
   $("#petForm").submit(function(event) {
     event.preventDefault();
     const nameInput = $('#name').val();
     userPet.name = nameInput;
     console.log(userPet);
     userPet.game(userPet);
+
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=portland&appid=c9e142f1ee0159d269d4a4a49598522a`;
+
+      request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          getElements(response);
+        }
+      }
+
+      request.open("GET", url, true);
+      request.send();
+
+      const getElements = function(response) {
+        $('#weather').show();
+        $('.showHumidity').text(`Humidity: ${response.main.humidity}%`);
+        $('.showTemp').text(`${Math.floor((response.main.temp - 273.15) + 32)} degrees`);
+      }
+      $('#weather').show();
   });
   $('#feed').click(function(){
-      userPet.feed();
+    userPet.feed();
   });
   $('#play').click(function(){
-      userPet.play();
+    userPet.play();
   });
 });
