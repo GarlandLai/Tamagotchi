ac80e333c61ecd67bd9b30bf29819b47d1730734
Alex Siegberg and Garland Lai <alex@>
2019-07-02 08:27:48 -0700

    add showinfo function to tamagotchi which will update meter levels

--- src/main.js
+++ src/main.js
@@ -4,21 +4,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export let showInfo = (pet) => {
-  $("#nameOutput").text(pet.name);
-  $("#energyOutput").text(pet.energy);
-  $("#fedOutput").text(pet.fed);
-  $("#happinessOutput").text(pet.happiness);
-}
-
 $(document).ready(function() {
   let userPet = new Pet;
   $("#petForm").submit(function(event) {
     event.preventDefault();
     const nameInput = $('#name').val();
     userPet.name = nameInput;
-    showInfo(userPet);
     console.log(userPet);
+    userPet.game();
   });
-  userPet.game();
 });
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 export class Pet {
   constructor(name) {
     this.name = name;
@@ -12,20 +14,20 @@ export class Pet {
     setInterval(() => {
       this.checkEnergy(1);
       this.checkFeed(-1);
-      this.checkPlay(-1);
+      this.checkHappiness(-1);
       this.checkHealth();
-      showInfo(userPet);
+      this.showInfo();
     }, 1000);
   }
 
   feed() {
     this.checkFeed(10);
-    this.checkPlay(5);
+    this.checkHappiness(5);
     this.checkEnergy(-15);
   }
 
   play() {
-    this.checkPlay(15);
+    this.checkHappiness(15);
     this.checkFeed(5);
     this.checkEnergy(-20);
   }
@@ -58,7 +60,7 @@ export class Pet {
     }
   }
 
-  checkPlay(value) {
+  checkHappiness(value) {
     if(this.happiness + value >= 100) {
       this.happiness = 100;
     } else if(this.happiness + value <= 0) {
@@ -66,6 +68,14 @@ export class Pet {
     } else {
       this.happiness += value;
     }
+  }
+
+  showInfo() {
+    $("#nameOutput").text(`Name: ${this.name}`);
+    $("#energyOutput").text(`Energy: ${this.energy}`);
+    $("#fedOutput").text(`Fed: ${this.fed}`);
+    $("#happinessOutput").text(`Happiness: ${this.happiness}`);
+    $("#petForm").hide();
   }
 
 }
